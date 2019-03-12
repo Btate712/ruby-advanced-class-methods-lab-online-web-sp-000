ba9c0d038101bfe6acf4fa0e4a623b4c6ff98259
Robert Tate <btate712@gmail.com>
2019-03-12 23:25:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
 
   def self.new_from_filename(filename)
     song_data = filename.split(" - ")
-    song_name = song_data[1].(0, song_data[1].length - 5)
+    song_name = song_data[1].slice(0, song_data[1].length - 5)
     binding.pry
     song = song.new_by_name(song_name)
     song.artist_name = song_data[0]

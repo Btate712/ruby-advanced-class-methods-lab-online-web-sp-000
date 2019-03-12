2c65f9624271f72342cdd29a236fe9f49afb0e3a
Robert Tate <btate712@gmail.com>
2019-03-12 22:56:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,31 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    all.each do |song|
+      if song.name = song_name
+        return song
+      end
+    end
+    return false
+  end
 end
